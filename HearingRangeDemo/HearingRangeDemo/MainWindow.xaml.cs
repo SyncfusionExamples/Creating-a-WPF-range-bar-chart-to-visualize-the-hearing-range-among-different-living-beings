31412bfcb5facc70cb93a879f1be24fcf3555e9c
saiyathalifathimabee.m@syncfusion.com <Saiyathalif@17>
2024-07-04 15:14:33 +0530

    Added the sample

---
+++ HearingRangeDemo/HearingRangeDemo/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Syncfusion.UI.Xaml.Charts;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HearingRangeDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LabelCreated(object sender, LabelCreatedEventArgs e)
+        {
+            double position = e.AxisLabel.Position;
+            if (position >= 1000 && position <= 180000)
+            {
+                string text = (position / 1000).ToString("N0");
+                e.AxisLabel.LabelContent = $"{text}kHz";
+            }
+            else
+            {
+                e.AxisLabel.LabelContent = $"{position:N0}Hz";
+            }
+        }
+    }
+}
---
+++ HearingRangeDemo/HearingRangeDemo/Model/HearingRangeModel.cs
@@ -0,0 +1,16 @@
+﻿namespace HearingRangeDemo
+{
+    public class HearingRangeModel
+    {
+        public string LivingBeings { get; set; }
+        public  double HighValue { get; set; }
+        public double LowValue { get; set; }
+
+        public HearingRangeModel(string livingBeings, double highValue, double lowValue)
+        {
+            LivingBeings = livingBeings;
+            HighValue = highValue;
+            LowValue = lowValue;
+        }
+    }
+}
